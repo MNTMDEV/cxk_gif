0966cdf9d536b546c1f0f123daf3a439ad900102
MNTMDEV <zfy@hrbeu.edu.cn>
2020-09-18 09:48:53 +0800

    code upload

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # cxk_gif
-cxk鸡你太美
+cxk鸡你太美动画linux版
+
+原图是这样子的：
+
+![图片](https://raw.githubusercontent.com/MNTMDEV/cxk_gif/master/pic/cxk.gif)
+
---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<stdio.h>
+#include<unistd.h>
+#include"payload.h"
+using namespace std;
+
+#define MAX_I		33
+#define MAX_J		60
+#define SLEEP_US	50000
+const int FRAMES=694;
+const char* SEG_TABLE="#RMNHQODBWGPZ*@$C&98?32I1>!:-;. ";
+void out_mat(int fr){
+	for(int i=0;i<MAX_I;i++){
+		for(int j=0;j<MAX_J;j++){
+			char data=payload[fr][i][j];
+			char content=SEG_TABLE[data];
+			cout<<content;
+		}
+		cout<<endl;
+	}
+}
+
+void disp(){
+	int i=0;
+	for(int i=0;i<FRAMES;i++){
+		//cursor move
+		cout<<"\33[0;0H";
+		//output
+		out_mat(i);
+		fflush(stdout);
+		usleep(SLEEP_US);
+	}
+}
+
+int main(){
+	cout<<"\033[2J";
+	disp();
+	return 0;
+}
